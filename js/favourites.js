96e1925d87d0c487d38772f246e425ca9953b21b
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-02-26 10:07:04 +0100

    Adding check if favorites are empty

--- js/favourites.js
+++ js/favourites.js
@@ -1,6 +1,19 @@
-let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
-
+const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
 const container = document.querySelector(".cards-container");
+const error = document.querySelector(".errorDisplay");
+
+if (favourites.length === 0) {
+	error.classList.add("error");
+	error.textContent = "No favourites city";
+	const backButton = document.createElement("button");
+	backButton.textContent = "Back";
+	backButton.onclick = function () {
+		window.location.href = "/weather-app/index.html";
+	};
+	backButton.classList.add("favouriteButton");
+	container.appendChild(backButton);
+}
+
 container.classList.add("card-container");
 
 favourites.forEach((city) => {
