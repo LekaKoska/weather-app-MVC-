50d3f06892b3da0f7896394f09f0633d28ed43c5
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-02-26 13:33:41 +0100

    * Implementing logic for favourites and unfavourites * Adding some css

--- js/favourites.js
+++ js/favourites.js
@@ -2,23 +2,26 @@ const favourites = JSON.parse(localStorage.getItem("favourites")) || [];
 const container = document.querySelector(".cards-container");
 const error = document.querySelector(".errorDisplay");
 
-if (favourites.length === 0) {
+container.classList.add("card-container");
+
+function renderEmpty() {
 	error.classList.add("error");
-	error.textContent = "No favourites city";
+	error.textContent = "No favourite cities";
+
 	const backButton = document.createElement("button");
 	backButton.textContent = "Back";
-	backButton.onclick = function () {
+	backButton.onclick = () => {
 		window.location.href = "/weather-app/index.html";
 	};
 	backButton.classList.add("favouriteButton");
+
 	container.appendChild(backButton);
 }
 
-container.classList.add("card-container");
-
-favourites.forEach((city) => {
+function renderCard(city) {
 	const card = document.createElement("div");
 	card.classList.add("card");
+	card.dataset.cityId = city.city_id; // za permalink kasnije
 
 	const cityDisplay = document.createElement("h1");
 	const descriptionDisplay = document.createElement("p");
@@ -31,4 +34,22 @@ favourites.forEach((city) => {
 
 	card.append(cityDisplay, descriptionDisplay);
 	container.appendChild(card);
-});
+}
+
+function attachCardClickListener() {
+	container.addEventListener("click", (e) => {
+		const card = e.target.closest(".card");
+		if (!card) return;
+
+		const cityId = card.dataset.cityId;
+		// TODO: otvori permalink za cityId
+		console.log("Clicked city id:", cityId);
+	});
+}
+
+if (favourites.length === 0) {
+	renderEmpty();
+} else {
+	favourites.forEach(renderCard);
+	attachCardClickListener();
+}
--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
 // TODO: localStorage, Toggle temp, Get Icon and Change backround
 // TODO: Create new file with favourites cities
+// TODO: Permalink
